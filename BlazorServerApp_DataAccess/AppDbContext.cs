82d66cbe82f84d3e3ed75c4da89626086fcafa57
mihaio89 <89@noreply.com>
2022-11-03 15:45:25 -0700

    db context

---
+++ BlazorServerApp_DataAccess/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerApp_DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // creates a table inn the database
+       // public DbSet<Domain>Domains { get; set; }
+}
